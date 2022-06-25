f3bfefc38af686a8b1c72ad47eb3e1b46a08f594
jgarc172 <jgarc172@users.noreply.github.com>
2022-06-25 19:32:23 +0000

    hello table tests

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -3,32 +3,28 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	t.Run("with name", func(t *testing.T) {
-		want := "Hello, Jose"
-		got := Hello("Jose", "")
-		if got != want {
-			t.Errorf("got %q  want %q", got, want)
-		}
-	})
-	t.Run("empty name", func(t *testing.T) {
-		want := "Hello, world"
-		got := Hello("", "")
-		if got != want {
-			t.Errorf("got %q  want %q", got, want)
-		}
-	})
-	t.Run("in Spanish", func(t *testing.T) {
-		want := "Hola, Jose"
-		got := Hello("Jose", "Spanish")
-		if got != want {
-			t.Errorf("got %q  want %q", got, want)
-		}
-	})
-	t.Run("in French", func(t *testing.T) {
-		want := "Bonjour, Jose"
-		got := Hello("Jose", "French")
-		if got != want {
-			t.Errorf("got %q  want %q", got, want)
-		}
-	})
+	for _, test := range cases() {
+		t.Run(test.name, func(t *testing.T) {
+			got := Hello(test.input1, test.input2)
+			if got != test.want {
+				t.Errorf("got %q  want %q", got, test.want)
+			}
+		})
+	}
+}
+
+type test struct {
+	name   string
+	input1 string
+	input2 string
+	want   string
+}
+
+func cases() []test {
+	return []test{
+		{"empty name", "", "", "Hello, world"},
+		{"empty lang", "Jose", "", "Hello, Jose"},
+		{"Spanish", "Jose", "Spanish", "Hola, Jose"},
+		{"French", "Jose", "French", "Bonjour, Jose"},
+	}
 }
