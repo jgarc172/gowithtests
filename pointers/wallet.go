103b3bb17c0b62cb02a85df58f0e3eb2818ef23d
jgarc172 <jgarc172@mail.ccsf.edu>
2022-07-10 04:58:44 +0000

    pointers overdraft

--- pointers/wallet.go
+++ pointers/wallet.go
@@ -1,1 +1,27 @@
 package wallet
+
+import "errors"
+
+type Bitcoin int
+
+type Wallet struct {
+	balance Bitcoin
+}
+
+func (w *Wallet) Balance() Bitcoin {
+	return w.balance
+}
+
+func (w *Wallet) Deposit(amount Bitcoin) {
+	w.balance += amount
+}
+
+func (w *Wallet) Withdraw(amount Bitcoin) error {
+	if amount > w.balance {
+		return ErrInsufficientFunds
+	}
+	w.balance -= amount
+	return nil
+}
+
+var ErrInsufficientFunds = errors.New("cannot withdraw, insufficient funds")
--- pointers/wallet_test.go
+++ pointers/wallet_test.go
@@ -5,47 +5,6 @@ import (
 	"testing"
 )
 
-func TestWallet(t *testing.T) {
-	wallet := Wallet{}
-	expected := Bitcoin(0)
-	got := wallet.Balance()
-	if got != expected {
-		t.Errorf("got '%v', expected '%v'", got, expected)
-	}
-
-	wallet.Deposit(Bitcoin(10))
-	expected = expected + Bitcoin(10)
-	got = wallet.Balance()
-	if got != expected {
-		t.Errorf("got '%v', expected '%v'", got, expected)
-	}
-
-	wallet.Withdraw(Bitcoin(5))
-	expected = expected - Bitcoin(5)
-	got = wallet.Balance()
-	if got != expected {
-		t.Errorf("got '%v', expected '%v'", got, expected)
-	}
-}
-
-type Bitcoin int
-
-type Wallet struct {
-	balance Bitcoin
-}
-
-func (w *Wallet) Balance() Bitcoin {
-	return w.balance
-}
-
-func (w *Wallet) Deposit(amount Bitcoin) {
-	w.balance += amount
-}
-
-func (w *Wallet) Withdraw(amount Bitcoin) {
-	w.balance -= amount
-}
-
 func ExampleWallet() {
 	w := Wallet{}
 	fmt.Println(w.Balance())
@@ -53,8 +12,54 @@ func ExampleWallet() {
 	fmt.Println(w.Balance())
 	w.Withdraw(Bitcoin(5))
 	fmt.Println(w.Balance())
+	err := w.Withdraw(Bitcoin(10))
+	fmt.Println(err)
 	// Output:
 	// 0
 	// 10
 	// 5
+	// cannot withdraw, insufficient funds
+}
+func TestWallet(t *testing.T) {
+	var wallet Wallet
+	var expected Bitcoin
+	var got Bitcoin
+	var err error
+
+	t.Run("new Wallet", func(t *testing.T) {
+		wallet = Wallet{}
+		expected = Bitcoin(0)
+		got = wallet.Balance()
+		if got != expected {
+			t.Errorf("got '%v', expected '%v'", got, expected)
+		}
+	})
+
+	t.Run("deposit", func(t *testing.T) {
+		wallet.Deposit(Bitcoin(10))
+		expected = expected + Bitcoin(10)
+		got = wallet.Balance()
+		if got != expected {
+			t.Errorf("got '%v', expected '%v'", got, expected)
+		}
+	})
+
+	t.Run("withdraw", func(t *testing.T) {
+		err = wallet.Withdraw(Bitcoin(5))
+		if err != nil {
+			t.Errorf("got '%v', expected '%v'", err, nil)
+		}
+		expected = expected - Bitcoin(5)
+		got = wallet.Balance()
+		if got != expected {
+			t.Errorf("got '%v', expected '%v'", got, expected)
+		}
+	})
+
+	t.Run("overdraft", func(t *testing.T) {
+		err = wallet.Withdraw(Bitcoin(10))
+		if err == nil {
+			t.Errorf("got '%v', expected '%v'", err, "cannot withdraw, insufficient funds")
+		}
+	})
 }
