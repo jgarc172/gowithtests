d4427c7760f7ae37bc2971c14bcde89a33ba3dd0
jgarc172 <jgarc172@mail.ccsf.edu>
2022-07-10 01:48:43 +0000

    structs shape area

--- structs/shapes.go
+++ structs/shapes.go
@@ -1,5 +1,37 @@
 package shapes
 
+import "math"
+
+type Shape interface {
+	Area() float64
+}
+
+type Rectangle struct {
+	width  float64
+	height float64
+}
+
+func (r Rectangle) Area() float64 {
+	return r.width * r.height
+}
+
+type Circle struct {
+	radius float64
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.radius * c.radius
+}
+
+type Triangle struct {
+	base   float64
+	height float64
+}
+
+func (t Triangle) Area() float64 {
+	return 0.5 * (t.base * t.height)
+}
+
 func Perimeter(width, height float64) (perimeter float64) {
 	if width < 0 || height < 0 {
 		perimeter = 0.0
--- structs/shapes_test.go
+++ structs/shapes_test.go
@@ -28,3 +28,29 @@ func casesPerimeter() (cases []testPerimeter) {
 		{"both positive", 4.2, 1.5, 11.4},
 	}
 }
+
+func TestArea(t *testing.T) {
+	for _, c := range casesArea() {
+		t.Run(c.name, func(t *testing.T) {
+			got := c.shape.Area()
+			if got != c.area {
+				t.Errorf("got '%f', but want '%f', given '%v'", got, c.area, c.shape)
+			}
+		})
+	}
+
+}
+
+type testArea struct {
+	name  string
+	shape Shape
+	area  float64
+}
+
+func casesArea() []testArea {
+	return []testArea{
+		{"rectangle", Rectangle{3.0, 2.0}, 6.0},
+		{"circle", Circle{10.0}, 314.1592653589793},
+		{"triangle", Triangle{12.0, 6.0}, 36.0},
+	}
+}
