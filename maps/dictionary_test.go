521a3a62e573a52f2c0cf00526bfd7369c50c252
jgarc172 <jgarc172@mail.ccsf.edu>
2022-07-10 19:21:44 +0000

    maps dictionary

--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -1,15 +1,45 @@
 package dictionary
 
-import "fmt"
+import (
+	"fmt"
+	"testing"
+)
 
-func ExampleSearch() {
-	dictionary := map[string]string{"test": "this is just a test"}
-	got := Search(dictionary, "test")
+func ExampleDictionary() {
+	dictionary := Dictionary{"test": "this is just a test"}
+	got := dictionary.Search("test")
 	fmt.Println(got)
 	// Output:
 	// this is just a test
 }
 
-func Search(dictionary map[string]string, s string) (value string) {
-	return dictionary[s]
+func TestDictionary(t *testing.T) {
+	for _, c := range cases() {
+		t.Run(c.name, func(t *testing.T) {
+			got := c.dict.Search(c.term)
+			if got != c.value {
+				t.Errorf("got '%v', expected '%v'", got, c.value)
+			}
+		})
+	}
+}
+
+type caseDictionary struct {
+	name  string
+	dict  Dictionary
+	term  string
+	value string
+}
+
+func cases() []caseDictionary {
+	return []caseDictionary{
+		{"exists", Dictionary{"test": "this is just a test"}, "test", "this is just a test"},
+		{"doesn't exist", Dictionary{}, "test", "t"},
+	}
+}
+
+type Dictionary map[string]string
+
+func (d Dictionary) Search(s string) (value string) {
+	return d[s]
 }
