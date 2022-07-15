924ef3be68fa1484cfae40032eb20db1451cfc84
jgarc172 <jgarc172@mail.ccsf.edu>
2022-07-15 22:19:55 +0000

    exec

---
+++ exec/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"io"
+	"os"
+	"os/exec"
+)
+
+func main() {
+	command := Command(os.Args)
+	err := Run(command, os.Stdout, os.Stderr)
+	if err != nil {
+		fmt.Printf("command '%v` failed: '%v'\n", command, err)
+		os.Exit(1)
+	}
+}
+
+func Command(args []string) (command []string) {
+	if len(args) < 2 {
+		command = []string{"echo", "No command provided"}
+		return
+	}
+	return args[1:]
+}
+
+func Run(command []string, o, e io.Writer) (err error) {
+	if len(command) < 1 {
+		return errors.New("no command provided")
+	}
+	cmd := exec.Command(command[0], command[1:]...)
+	cmd.Stdout = o
+	cmd.Stderr = e
+	err = cmd.Run()
+	return
+}
---
+++ exec/main_test.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"bytes"
+	"errors"
+	"io"
+	"os"
+	"reflect"
+	"testing"
+)
+
+func ExampleRun() {
+	args := []string{"echo", "hello world"}
+	Run(args, os.Stdout, os.Stderr)
+	args = []string{"ls"}
+	Run(args, os.Stdout, os.Stderr)
+	// Output:
+	// hello world
+	// main.go
+	// main_test.go
+}
+
+func TestCommand(t *testing.T) {
+	for _, c := range casesCommand() {
+		t.Run(c.name, func(t *testing.T) {
+			command := Command(c.args)
+			if !reflect.DeepEqual(command, c.expect) {
+				t.Errorf("got '%v', expected '%v'", command, c.expect)
+			}
+		})
+	}
+}
+
+type testCommand struct {
+	name   string
+	args   []string
+	expect []string
+}
+
+func casesCommand() []testCommand {
+	return []testCommand{
+		{"no command", []string{"main"}, []string{"echo", "No command provided"}},
+		{"simple", []string{"main", "ls"}, []string{"ls"}},
+		{"longer", []string{"main", "echo", "one", "two"}, []string{"echo", "one", "two"}},
+	}
+}
+
+func TestRun(t *testing.T) {
+	for _, c := range casesRun() {
+		t.Run(c.name, func(t *testing.T) {
+			err := Run(c.command, c.out, c.errOut)
+			if err != nil && c.expect != nil {
+				if !(len(err.Error()) > 0 && len(c.expect.Error()) > 0) {
+					t.Errorf("got '%v', expected '%v'", err, c.expect)
+				}
+			}
+		})
+	}
+}
+
+type testRun struct {
+	name    string
+	command []string
+	out     io.Writer
+	errOut  io.Writer
+	expect  error
+}
+
+func casesRun() []testRun {
+	return []testRun{
+		{"success", []string{"ls", "-l"}, newWriter(), newWriter(), nil},
+		{"unknown command", []string{"notacommand", "-l"}, newWriter(), newWriter(), errors.New("error")},
+		{"empty command", []string{}, newWriter(), newWriter(), errors.New("error")},
+	}
+}
+
+func newWriter() *bytes.Buffer {
+	return &bytes.Buffer{}
+}
